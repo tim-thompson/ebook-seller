f7fce95a23dcc3e0f8017e04ec72e1d4e82a4a1a
Tim Thompson <tim.thompson147@gmail.com>
2016-11-24 17:59:12 +0000

    Download checking and functionality

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
+from flask import send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+
+
+class Purchase(db.Model):
+    __tablename__ = "purchases"
+    uuid = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String)
+    downloads_left = db.Column(db.Integer)
 
 
 @app.route("/")
@@ -10,12 +18,24 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/purchase", methods=["POST"])
+def purchase():
+    return "purchase"
+
+
+@app.route("/<uuid>")
+def download(uuid):
+    purchase = Purchase.query.get(uuid)
+    if purchase:
+        if purchase.downloads_left <= 0:
+            return "You have no downloads left"
+        else:
+            purchase.downloads_left -= 1
+            db.session.commit()
+            return send_from_directory("downloads", "book.pdf")
+    else:
+        abort(404)
+
+
 if __name__ == "__main__":
     app.run()
-
-
-class Purchase(db.table):
-    __tablename__ = "purchases"
-    uuid = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String)
-    downloads_left = db.Column(db.Integer)
