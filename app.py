6417c9f2bfbd08f8091561fc22fd452e00026ff4
Tim Thompson <tim.thompson147@gmail.com>
2016-11-24 17:29:59 +0000

    Basic purchase model created

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
@@ -7,3 +10,9 @@ def index():
 
 if __name__ == "__main__":
     app.run()
+
+class Purchase(db.table):
+    __tablename__ = "purchases"
+    uuid = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String)
+    downloads_left = db.Column(db.Integer)
