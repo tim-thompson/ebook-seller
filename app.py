8a1633fc9c44e90c2b23f6150678c542d830d435
Tim Thompson <tim.thompson147@gmail.com>
2016-11-24 17:37:14 +0000

    PEP8 compliance

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 if __name__ == "__main__":
     app.run()
+
 
 class Purchase(db.table):
     __tablename__ = "purchases"
